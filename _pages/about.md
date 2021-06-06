06993b7b9ffc5ffecf8098b40884a2fc98fcd7ed
Arnau Gàmez i Montolio <arnaugamez@protonmail.com>
2021-06-06 21:19:34 +0200

    Update web

--- _pages/about.md
+++ _pages/about.md
@@ -1,22 +1,15 @@
 ---
 permalink: /about/
 title: "About"
-classes: wide
 ---
 
-Hi there! I am _Arnau Gàmez i Montolio_, a reverse engineer and mathematician from lovely Barcelona.
+Hi there! I am _Arnau Gàmez i Montolio_, a catalan hacker, reverse engineer and mathematician.
 
-My interests are broad, but recently I have been particularly focused on:
-- Code (de)obfuscation
+I do security research within a broad range of interests, but recently I have been particularly focused in:
 - Malware analysis
-- Binary formats
-- Cryptography
+- Code (de)obfuscation
+- Mixed Boolean-Arithmetic (MBA) expressions
 
 Co-founder and president of [Hacking Lliure](https://twitter.com/HackingLliure), a catalan non-profit association about digital rights, open source and security awareness.
 
-I also hold a double degree in Mathematics and Computer Engineering.
-
 [![Twitter Follow @arnaugamez](https://img.shields.io/twitter/follow/arnaugamez?style=social)](https://twitter.com/intent/follow?screen_name=arnaugamez)
-
-
-
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,60 +2,60 @@ var store = [{
         "title": "Hello world!",
         "excerpt":"Hello world!  ","categories": ["Blog"],
         "tags": ["Hello"],
-        "url": "http://localhost:4000/blog/2019/07/25/hello-world/",
+        "url": "/blog/2019/07/25/hello-world/",
         "teaser": null
       },{
         "title": "Digging Deeper into Gmail URLs",
         "excerpt":"Find my published research on extracting timestamps and other information from Gmail URLs with the accompanying tool Gmail URL Decoder that I developed during my DFIR internship at Arsenal.  ","categories": ["Blog"],
         "tags": ["DFIR","gmail","URL"],
-        "url": "http://localhost:4000/blog/2019/07/29/digging-deeper-gmail-urls/",
-        "teaser": "http://localhost:4000/assets/images/posts/GmailURLdecoder.jpg"
+        "url": "/blog/2019/07/29/digging-deeper-gmail-urls/",
+        "teaser": "/assets/images/posts/GmailURLdecoder.jpg"
       },{
         "title": "Solving LMFAO stego challenge",
         "excerpt":"Last week I attended the HackUPC at Barcelona. They were running a 24h CTF-like competition with different challenges focused on algorithms, programming and some hacking. I wasn’t planning to play, but some friends got me involved in the last moment and I somehow managed to win the competition by completing...","categories": ["Blog"],
         "tags": ["hackupc","writeup","stego","steganography","thegame"],
-        "url": "http://localhost:4000/blog/2019/10/21/hackupc-lmfao-writeup/",
-        "teaser": "http://localhost:4000/assets/images/posts/LMFAO.png"
+        "url": "/blog/2019/10/21/hackupc-lmfao-writeup/",
+        "teaser": "/assets/images/posts/LMFAO.png"
       },{
         "title": "Solving sonda reversing challenge with radare2",
         "excerpt":"This was the reversing challenge from the HackUPC TheGame CTF-like competition. A slightly modified version has also been used recently during the Advent of Corona running platform of challenges during the 2020 covid19 confinement. Description We are encouraged to download a file named sonda referred as SONDA.EXE, so it seems...","categories": ["Blog"],
         "tags": ["thegame","sonda","hackupc","radare2","esil","emulation","reversing","r2pipe"],
-        "url": "http://localhost:4000/blog/2020/04/03/hackupc-sonda-writeup/",
-        "teaser": "http://localhost:4000/assets/images/posts/sonda_bb_main_3.png"
+        "url": "/blog/2020/04/03/hackupc-sonda-writeup/",
+        "teaser": "/assets/images/posts/sonda_bb_main_3.png"
       },{
         "title": "Solving lagrange reversing challenge with Cutter",
         "excerpt":"The Lagrange reversing challenge was the 6th problem included in the Advent of Corona CTF platform. The platform was uploading challenges during the first weeks of covid19 confinement, and it is still up if you want to take a look and play. Description We download the binary file lagrange_baby from...","categories": ["Blog"],
         "tags": ["radare2","reversing","Cutter","hackupc","advent"],
-        "url": "http://localhost:4000/blog/2020/04/12/advent-solve-lagrange-cutter/",
-        "teaser": "http://localhost:4000/assets/images/posts/lagrange_cutter_6.png"
+        "url": "/blog/2020/04/12/advent-solve-lagrange-cutter/",
+        "teaser": "/assets/images/posts/lagrange_cutter_6.png"
       },{
         "title": "Write-up for FlareOn7 challenge #1 - fidler",
         "excerpt":"Description We are introduced to the challenge with the following message: Welcome to the Seventh Flare-On Challenge! This is a simple game. Win it by any means necessary and the victory screen will reveal the flag. Enter the flag here on this site to score and move on to the...","categories": ["Blog"],
         "tags": ["flareon","reversing","python","CTF"],
-        "url": "http://localhost:4000/blog/2020/10/23/writeup-flareon7-challenge1/",
+        "url": "/blog/2020/10/23/writeup-flareon7-challenge1/",
         "teaser": null
       },{
         "title": "Write-up for FlareOn7 challenge #2 - garbage",
         "excerpt":"Description We are introduced to the challenge with the following message: One of our team members developed a Flare-On challenge but accidentally deleted it. We recovered it using extreme digital forensic techniques but it seems to be corrupted. We would fix it but we are too busy solving today’s most...","categories": ["Blog"],
         "tags": ["flareon","reversing","radare2","emulation","esil","CTF"],
-        "url": "http://localhost:4000/blog/2020/10/24/writeup-flareon7-challenge2/",
-        "teaser": "http://localhost:4000/assets/images/posts/flareon7_ch2/upx_bad.png"
+        "url": "/blog/2020/10/24/writeup-flareon7-challenge2/",
+        "teaser": "/assets/images/posts/flareon7_ch2/upx_bad.png"
       },{
         "title": "Write-up for FlareOn7 challenge #3 - wednesday",
         "excerpt":"Description We are introduced to the challenge with the following message: Be the wednesday. Unlike challenge 1, you probably won’t be able to beat this game the old fashioned way. Read the README.txt file, it is very important. If we open the README.txt file, we find the following: --- BE...","categories": ["Blog"],
         "tags": ["flareon","reversing","CTF","IDA","x64dbg","game"],
-        "url": "http://localhost:4000/blog/2020/11/07/writeup-flareon7-challenge3/",
-        "teaser": "http://localhost:4000/assets/images/posts/flareon7_ch3/general_collision_check.png"
+        "url": "/blog/2020/11/07/writeup-flareon7-challenge3/",
+        "teaser": "/assets/images/posts/flareon7_ch3/general_collision_check.png"
       },{
         "title": "Write-up for FlareOn7 challenge #4 - report",
         "excerpt":"Description We are introduced to the challenge with the following message: Nobody likes analysing infected documents, but it pays the bills. Reverse this macro thrill-ride to discover how to get it to show you the key. There is a single file report.xls to analyze, which is an excel document with...","categories": ["Blog"],
         "tags": ["flareon","reversing","CTF","macros","excel"],
-        "url": "http://localhost:4000/blog/2020/12/12/writeup-flareon7-challenge4/",
-        "teaser": "http://localhost:4000/assets/images/posts/flareon7_ch4/bypass_protections.png"
+        "url": "/blog/2020/12/12/writeup-flareon7-challenge4/",
+        "teaser": "/assets/images/posts/flareon7_ch4/bypass_protections.png"
       },{
         "title": "Write-up for FlareOn7 challenge #5 - TKApp",
         "excerpt":"Description  We are introduced to the challenge with the following message:      Now you can play Flare-On on your watch! As long as you still have an arm left to put a watch on, or emulate the watch’s operating system with sophisticated developer tools.    Analysis   Retrieve flag  ","categories": ["Blog"],
         "tags": ["flareon","reversing","CTF","dotPeek",".NET"],
-        "url": "http://localhost:4000/blog/2020/12/18/writeup-flareon7-challenge5/",
-        "teaser": "http://localhost:4000/assets/images/posts/flareon7_ch5/TODO.png"
+        "url": "/blog/2020/12/18/writeup-flareon7-challenge5/",
+        "teaser": "/assets/images/posts/flareon7_ch5/TODO.png"
       }]
